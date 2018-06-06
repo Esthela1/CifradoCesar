4c8ce6e569cdf2ca7a284921a21618335b81c9d8
jetzable <gjetzable@gmail.com>
2018-06-06 10:08:26 -0500

    Función encriptar agregada

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 let movimiento = document.getElementById("desplazamiento");
 let texto = document.getElementById("firstMessage");
+let resultado = document.getElementById("resultado");
 let message = [];
 let newMessage = [];
-let finalMessage;
+let finalMessage = [];
 
 function encriptar(){
 message = texto.value;
 for(i=0;i<message.length; i++){
 	newMessage[i] = (((message.charCodeAt(i) - 65) + movimiento.value)%26) + 65;
+	finalMessage[i]= String.fromCharCode(newMessage[i]);
 }
-
-
-/*for(i=0;i<newMessage.length;i++){
-	finalMessage[i] = message.fromCharCode(newMessage[i]);
-}*/
+resultado.innerHTML += finalMessage;
 console.log(newMessage);
 console.log(finalMessage);
 }
