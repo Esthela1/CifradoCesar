b8ef1d705fc5597e3563ceb6b967e80c53157f77
jetzable <gjetzable@gmail.com>
2018-06-07 12:20:40 -0500

    Cifrado funcionando al 100%, falta window.cipher

--- app.js
+++ app.js
@@ -1,42 +1,58 @@
 let movimiento = document.getElementById("desplazamiento");
 let texto = document.getElementById("firstMessage");
 let resultado = document.getElementById("resultado");
-let message = [];
+let message;
 let newMessage = [];
 let finalMessage = [];
+resultado.innerHTML += " ";
 
 function encriptar(){
 	message = texto.value;
+	resultado.innerHTML = " ";
+	let delta = parseInt(movimiento.value) % 26;
+	console.log(delta);
 	for(i=0;i<message.length; i++){
 		if (message.charCodeAt(i)>90) {
-			newMessage[i] = ((message.charCodeAt(i) - 97 + parseInt(movimiento.value))%26) + 97;
-			console.log(newMessage);
+			newMessage[i] = ((message.charCodeAt(i) - 97 + delta)%26) + 97;
 			finalMessage[i] = String.fromCharCode(newMessage[i]);
 		}
 		else{
-		newMessage[i] = ((message.charCodeAt(i) - 65 + parseInt(movimiento.value))%26) + 65;
-		console.log(newMessage);
-		finalMessage[i] = String.fromCharCode(newMessage[i]);
+			newMessage[i] = ((message.charCodeAt(i) - 65 + delta)%26) + 65;
+			finalMessage[i] = String.fromCharCode(newMessage[i]);
+		}
+		resultado.innerHTML += finalMessage[i];
 	}
-	}
-	resultado.innerHTML += finalMessage;
-	console.log(finalMessage);
 }
 
 function desencriptar(){
 	message = texto.value;
+	resultado.innerHTML = " ";
+	let delta = parseInt(movimiento.value) % 26;
+	console.log(delta);
 	for(i=0;i<message.length; i++){
 		if (message.charCodeAt(i)>90) {
-			newMessage[i] = ((message.charCodeAt(i) - 97 - parseInt(movimiento.value))%26) + 97;
-			console.log(newMessage);
-			finalMessage[i] = String.fromCharCode(newMessage[i]);
+
+			if((message.charCodeAt(i) - 97 - delta)<0){
+				newMessage[i] = ((message.charCodeAt(i) - 97 - delta)%26) + 123;
+				finalMessage[i] = String.fromCharCode(newMessage[i]);
+				console.log(newMessage[i]);
+			}
+			else{
+				newMessage[i] = ((message.charCodeAt(i) - 97 - delta)%26) + 97;
+				finalMessage[i] = String.fromCharCode(newMessage[i]);
+				console.log(newMessage[i]);
+			}
 		}
 		else{
-		newMessage[i] = ((message.charCodeAt(i) - 65 - parseInt(movimiento.value))%26) + 65;
-		console.log(newMessage);
-		finalMessage[i] = String.fromCharCode(newMessage[i]);
+			if ((message.charCodeAt(i) - 97 - delta)<0) {
+				newMessage[i] = ((message.charCodeAt(i) - 65 - delta)%26) + 91;
+				finalMessage[i] = String.fromCharCode(newMessage[i]);
+			}
+			else{
+				newMessage[i] = ((message.charCodeAt(i) - 65 + delta)%26) + 65;
+				finalMessage[i] = String.fromCharCode(newMessage[i]);
+			}
+		}
+		resultado.innerHTML += finalMessage[i];
 	}
-	}
-	resultado.innerHTML += finalMessage;
-	console.log(finalMessage);
-	}
+}
